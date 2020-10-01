d58d4e558981fcd924e52574d86db122113d9191
Carlos Lopez <60913078+cardanxd@users.noreply.github.com>
2020-10-01 17:21:05 -0500

    Commit 2.0

--- Ejercicio 3/js/codigo.js
+++ Ejercicio 3/js/codigo.js
@@ -1,40 +1,73 @@
-function OnGet(numeros) {
-    var parcial = {
-        arreglo: [0, 0, 0, 0, 0],
-        total: 0
-    };
+function GetSuma() {
+    var suma = new Array[2][2];
 
     for (var i = 0; i < numeros.length; i++) {
         for (var e = 0; e < numeros[i].length; e++) {
             var arreglo = numeros[i][e];
 
-            parcial.arreglo[e] += arreglo;
-
-            if (arreglo == 0)
-                parcial.total++;
-
-            
+            suma = parcial.arreglos[i][e] + parcial.arreglos[i][e];
 
         }
     }
 
-    return parcial;
+    return suma;
+}
+
+function GetResta(arreglos){
+    var resta = new Array[2][2];
+
+    for (var i = 0; i < numeros.length; i++) {
+        for (var e = 0; e < numeros[i].length; e++) {
+            var arreglo = numeros[i][e];
+
+            resta = parcial.arreglos[i][e] - parcial.arreglos[i][e];
+
+        }
+    }
+
+    return resta;
+}
+
+function GetProducto(arreglos){
+    var producto = new Array[2][2];
+
+    for (var i = 0; i < numeros.length; i++) {
+        for (var e = 0; e < numeros[i].length; e++) {
+            var arreglo = numeros[i][e];
+
+            producto = parcial.arreglos[i][e] * parcial.arreglos[i][e];
+
+        }
+    }
+    return producto;
+}
+
+function GetDivision(arreglo){
+    var div = new Array[2][2];
+
+    for (var i = 0; i < numeros.length; i++) {
+        for (var e = 0; e < numeros[i].length; e++) {
+            var arreglo = numeros[i][e];
+
+            div = parcial.arreglos[i][e] / parcial.arreglos[i][e];
+
+        }
+    }
+
+    return div;
 }
 
 function InfoPartials() {
-    var arreglos = [
-        [0, 2, 5, 7, 6],
-        [0, 0, 0, 3, 8],
-        [2, 9, 6, 3, 4],
-        [1, 5, 6, 1, 4], 
-        [0, 9, 2, 5, 0]
-
-    ];
+    
+    var arreglos = [[10, 5],[8, 2],[2,3],[6,8]];
 
     var ceros = OnGet(arreglos);
 
     document.getElementById("test").innerHTML += "Resultado <br>";
 
-    document.getElementById("test").innerHTML += "Total de ceros: " + ceros.total + "<br>";
+    document.getElementById("test").innerHTML += "Suma: " + ceros.suma + "<br>";
+    document.getElementById("test").innerHTML += "Resta: " + ceros.resta + "<br>";
+    document.getElementById("test").innerHTML += "Producto: " + ceros.producto + "<br>";
+    document.getElementById("test").innerHTML += "Divison: " + ceros.div + "<br>";
 
 }
