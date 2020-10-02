f581fd340e573d998aa9e674fcf78dd3b2245920
kevin Monsreal <kevinmonsreal99@gmail.com>
2020-10-01 20:32:47 -0500

    rayos2

--- js/actividad 5.js
+++ js/actividad 5.js
@@ -1,18 +1,44 @@
-function matriz() {
+#include <iostream>
 
-    for (j = 0; j < 10; j++) {
-        sum = 0;
-        for (i = 0; i < 10; i++) {
-            a[i][j] = (i * 10) + i + j;
-            sum += a[i][j];
-        }
-        a[10][j] = sum;
-    }
+using namespace std;
+void main()
+{
+	float datos[5][10], suma = 0, prom = 0;
+	int i, j;
+	//ingresar datos a la matriz
+	for (i = 0; 1 < 5; i++) 
+	{
+		for (j = 0; j < 10; j++)
+		{
+			cout << "ingresar datos a la matriz" << i + 1 << "," << j + 1 << ":\t";
+			cin >> datos[i][j];
+		}
+	}
+	cout << "la suma y el promedio de los datos de la matriz - fila es: " << endl;
+	for (i = 0; i < 5; i++) 
+	{
+		suma = 0;
+		for (j = 0; j < 10; j++) 
+		{
+			suma = suma + datos[i][j];
+		}
+		prom = suma / j;
+		cout << "la suma de los datos " << i + 1 << " es: " << suma << endl;
+		cout << "el promedio es: " << prom << endl;
+	}
+	cout << endl << endl;
+	cout << "la suma y el promedio de los datos de la matriz - columna es: " << endl;
+	for (j = 0; j < 10; j++) 
+	{
+		suma = 0;
+		for (i = 0; i < 5; i++) 
+		{
+			suma = suma + datos[i][j];
+		}
+		prom = suma / i;
+		cout << "la suma de los datos B" << j + 1 << " es: " << suma << endl;
+		cout << "el promedio es: " << prom << endl;
+	}
+	system("pause"); 
 
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 10; j++) {
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
 }
